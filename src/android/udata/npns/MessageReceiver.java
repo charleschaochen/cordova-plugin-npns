175d92f769e7df775401ba82d6baf34973d4a169
陈超 <ccn1069@corp.netease.com>
2015-09-21 18:31:04 +0800

    fix plugin bug

---
+++ src/android/udata/npns/MessageReceiver.java
@@ -0,0 +1,89 @@
+package udata.npns;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import com.netease.npnssdk.interfaces.BaseReceiver;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import org.apache.cordova.api.PluginResult;
+import org.json.JSONObject;
+
+
+/**
+ * Receiver to receive push messages or registration state.
+ * @author maotianjiao (gzmaotianjiao@corp.netease.com)
+ *
+ */
+public class MessageReceiver extends BaseReceiver {
+
+    @Override
+    protected void onPushNotificationReceived(Context context, int msgId, String payload) {
+        // If you want show the notification in your own way, remove the super call.
+    }
+    
+    @Override
+    protected void onUserDefinedMessageReceived(Context context, int msgId, String payload) {
+    }
+    
+    @Override
+    protected void onRegistrationStateReceived(final Context context, int statusCode, String deviceToken) {
+        super.onRegistrationStateReceived(context, statusCode, deviceToken);
+        
+        String message = "";
+        if (statusCode == 0) {
+            message = "register to NPNS server succeed! token=" + deviceToken;
+
+            /** Set the alias name.
+            NpnsSdkInterface.setAlias("npns_123", new AliasCallback() {
+
+                @Override
+                public void onResult(int statusCode, String alias) {
+                    String message = "";
+                    if (statusCode == AliasCallback.SUCCEED) {
+                        message = "set alias succeed! alias = " + alias;
+                    } else {
+                        message = "set alias failed! code=" + statusCode;
+                    }
+                    updateAndSaveMessage(context, message);
+                }
+            });
+            */
+
+            JSONObject obj = new JSONObject();
+            obj.put("token", deviceToken);
+            obj.put("msg", message);
+            NeteasePush.pushContext.success(obj);
+        } else {
+            message = "register to NPNS server failed!";
+            NeteasePush.pushContext.error(message);
+        }
+        
+//        updateAndSaveMessage(context, message);
+    }
+
+    @Override
+    protected void onConnectionStateReceived(Context context, boolean connected) {
+    }
+    
+    private void updateAndSaveMessage(Context context, String message) {
+//        SimpleDateFormat sdf = new SimpleDateFormat("[ MM-dd HH:mm:ss ]", Locale.CHINA);
+//        String currentDateandTime = sdf.format(new Date());
+//        message = currentDateandTime + "\n" + message + "\n\n";
+//
+//        Intent intent = new Intent(MainActivity.INTENT_MESSAGE_RECEIVED);
+//        intent.putExtra(MainActivity.EXTRA_MESSAGE, message);
+//        LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+//
+//        File file = new File(context.getFilesDir(), MainActivity.MESSAGE_FILE);
+//        String oldMessage = MainActivity.readFromFile(file);
+//        if (oldMessage != null) {
+//            message = message + oldMessage;
+//        }
+//        MainActivity.writeToFile(file, message);
+    }
+}
---
+++ src/android/udata/npns/NeteasePush.java
@@ -0,0 +1,35 @@
+package udata.npns;
+
+import com.netease.npnssdk.interfaces.NpnsSdkInterface;
+import org.apache.cordova.CallbackContext;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+
+public class NeteasePush extends PushPlugin {
+    private static final String APP_ID = "55ffaf15eb20962673e2b391";
+    private static final String TICKET = "1:1442819861:655d35d25dcf19d4:3020d5414167f6899767b8fbb66f5c72df892f35";
+
+    private static final String TAG = "NeteasePushPlugin";
+
+   public static CallbackContext pushContext;
+
+    @Override
+    protected boolean registerPush(Context context, String alias, CallbackContext callback) {
+        pushContext = callback;
+        Log.d(TAG, "> register: " + alias);
+        NpnsSdkInterface.registerPush(context, APP_ID, TICKET, MessageReceiver.class);
+        return true;
+    }
+
+    @Override
+    protected boolean unregisterPush(Context context, CallbackContext callback) {
+        return true;
+    }
+
+    @Override
+    protected boolean addListener(Context context, CallbackContext callback) {
+    }
+
+}
